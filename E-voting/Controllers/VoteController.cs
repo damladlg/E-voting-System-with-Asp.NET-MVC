2b3c9b8f184ec6e65421291d4306c2dd0632eac0
Damla Dalgıç <damlabileydi@gmail.com>
2020-12-16 18:09:58 +0300

    theme integrated

---
+++ E-voting/Controllers/VoteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_voting.Controllers
+{
+    public class VoteController : Controller
+    {
+        // GET: Vote
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
