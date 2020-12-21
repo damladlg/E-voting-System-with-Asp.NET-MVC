43f28aca4dc94e81c4455a21a38835b0f3720889
Damla Dalgıç <damlabileydi@gmail.com>
2020-12-21 14:57:14 +0300

    login screen created

--- E-voting/Controllers/AdminController.cs
+++ E-voting/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using E_voting.Models;
 using E_voting.Models.DataContext;
+using E_voting.Models.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,32 @@ namespace E_voting.Controllers
         {
             var sorgu = db.Voter.ToList();
             return View(sorgu);
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Admin admin)
+        {
+            var login = db.Admin.Where(x => x.Email == admin.Email).SingleOrDefault();
+            if ((login.Email == admin.Email) && (login.Password == admin.Password))
+            {
+                Session["adminid"] = login.AdminId;
+                Session["email"] = login.Email;
+                return RedirectToAction("Index", "Admin");
+
+            }
+            ViewBag.Uyari = "Wrong password or name";
+            return View(admin);
+
+        }
+        public ActionResult Logout()
+        {
+            Session["adminid"] = null;
+            Session["email"] = null;
+            Session.Abandon();
+            return RedirectToAction("Login", "Admin");
         }
     }
 }
