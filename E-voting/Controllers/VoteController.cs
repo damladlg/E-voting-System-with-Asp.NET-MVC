530f3baa224db8a435d2cca5f82f38accae2bc41
Damla Dalgıç <damlabileydi@gmail.com>
2020-12-26 23:00:46 +0300

    Voting screen created

--- E-voting/Controllers/VoteController.cs
+++ E-voting/Controllers/VoteController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using E_voting.Models;
+using E_voting.Models.DataContext;
+using E_voting.Models.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,17 @@ namespace E_voting.Controllers
 {
     public class VoteController : Controller
     {
+        EvotingDBContext db = new EvotingDBContext();
         // GET: Vote
         public ActionResult Index()
         {
+            // ViewBag.Candidate = db.Candidate.ToList().OrderByDescending(x => x.CandidateId);
             return View();
         }
+        public ActionResult VoteNow()
+        {
+            return View(db.Candidate.ToList().OrderByDescending(x => x.CandidateId));
+        }
     }
+  
 }
